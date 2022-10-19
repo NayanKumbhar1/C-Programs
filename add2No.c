6507980624598bdf2abd6a4ce9b9259b8023460f
NayanKumbhar1 <nayankumbhar1515@gmail.com>
2022-10-19 09:36:34 +0530

    [ADDED]:Tahir C Code for Addition of 2 No

---
+++ add2No.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c=0;
+	printf("\nEnter the First Number:");
+	scanf("%d",&a);
+	printf("\nEnter the Second Number:");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\nAddition=%d",c);
+}
